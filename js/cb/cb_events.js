77b0f23363b14af875f6e9df704a000939c8708a
Adamandom <74009107+Adamandom@users.noreply.github.com>
2022-07-19 01:00:52 +0100

    19/07/22 Added favicon. HTH: Added player walk off.

--- js/cb/cb_events.js
+++
@@ -1,9 +0,0 @@
-/******************************************
-*     Page Navigation Buttons
-******************************************/
-prevPage.addEventListener( "click", () => {
-  window.location.href = "fc.html"
-} )
-nextPage.addEventListener( "click", () => {
-  window.location.href = "hth.html"
-} )
